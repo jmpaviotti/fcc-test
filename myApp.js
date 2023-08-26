48e62ecc6d471d6460a2c9f585dd88752d3820ff
Julian <jmpaviotti@gmail.com>
2023-08-26 18:38:32 -0300

    .

--- myApp.js
+++ myApp.js
@@ -1,13 +1,11 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello World");
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/views/index.html')
 })
-
-
 
 
 
