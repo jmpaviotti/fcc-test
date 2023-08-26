ab1228830cda22c6114cbd173160a03a7aa378f6
Julian <jmpaviotti@gmail.com>
2023-08-26 20:19:28 -0300

    .

--- myApp.js
+++ myApp.js
@@ -3,10 +3,15 @@ require('dotenv').config();
 let express = require('express');
 let app = express();
 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+})
+
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html')
+    res.sendFile(__dirname + '/views/index.html');
 })
 
 app.get('/json', (req, res) => {
